bcf09e87d1fdbac8914a4036e4b6c4a7fab253b7
emmanuel <emmanuelchukwuemeka970@gmail.com>
2023-06-07 08:48:59 +0100

    first commit

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+    return ( 
+        <nav>
+            <h1>header</h1>
+        </nav>
+     );
+}
+ 
+export default Header;
---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return ( 
+        <footer>
+            <p>Please this application can only make 100 requests a day, if you check it and it`s broken, please check it out the next day. </p>
+            <p>&copy; <a href="https://www.linkedin.com/in/ewenike-chukwuemeka-emmanuel-2b2673231/" title="Check my profile">Ewenike Chukwuemeka Emmanuel</a></p>
+
+        </footer>
+     );
+}
+ 
+export default Footer;
